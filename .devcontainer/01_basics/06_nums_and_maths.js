a71727062da972006a8c54766907d22414745e9a
iroshanay <155223560+iroshanay@users.noreply.github.com>
2024-11-15 07:04:16 +0000

    date&time

--- .devcontainer/01_basics/06_nums_and_maths.js
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -23,15 +23,15 @@ const hundreds = 1000000
 // console.log(Math.floor(4.2));
 // console.log(Math.min(2,3,4,5));
 // console.log(Math.max(2,3,4,5));
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log(Math.random());
+//  console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
-const min = 10
+//  const min = 10
 
-const max = 20
+//  const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+//  console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 
 
 
