24d7bc5eb104cb69f857acda830adaa1839d8084
iroshanay <155223560+iroshanay@users.noreply.github.com>
2024-11-15 21:06:57 +0000

    array

---
+++ .devcontainer/02_basics/02_array.js
@@ -0,0 +1,26 @@
+const marvel_heros = ["thor","Ironman","spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+//marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+//  console.log(marvel_heros[3][1]);
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+// const allHeros = [...marvel_heros, ...dc_heros]
+// console.log(allHeros);
+
+// const anotherArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = anotherArray.flat(Infinity)
+// console.log(real_another_array);
+
+console.log(Array.isArray("Roshanay"));
+console.log(Array.from("Roshanay"));
+console.log(Array.from({name: "Roshanay"}));// interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
+
+
+
+ 
